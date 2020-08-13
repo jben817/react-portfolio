e757292d04b13957b4ad1afec0fc448dbd7cbefc
j.bennett <61255945+jben817@users.noreply.github.com>
2020-08-13 17:22:06 -0400

    Updated links in navbar

--- src/components/Navbartab.js
+++ src/components/Navbartab.js
@@ -6,13 +6,13 @@ function Navbartab(props) {
     return (
         <div>
             <ReactBootStrap.Navbar bg="primary" expand="lg">
-                <ReactBootStrap.Navbar.Brand href="/">Portfolio</ReactBootStrap.Navbar.Brand>
+                <ReactBootStrap.Navbar.Brand href="/react-portfolio">Portfolio</ReactBootStrap.Navbar.Brand>
                 <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
                     <ReactBootStrap.Nav className="mr-auto">
-                        <ReactBootStrap.Nav.Link href="/">About</ReactBootStrap.Nav.Link>
-                        <ReactBootStrap.Nav.Link href="/projects">Portfolio Projects</ReactBootStrap.Nav.Link>
-                        <ReactBootStrap.Nav.Link href="/contact">Contact Me</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="/react-portfolio">About</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="/react-portfolio/projects">Portfolio Projects</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="/react-portfolio/contact">Contact Me</ReactBootStrap.Nav.Link>
                     </ReactBootStrap.Nav>
                 </ReactBootStrap.Navbar.Collapse>
             </ReactBootStrap.Navbar>
