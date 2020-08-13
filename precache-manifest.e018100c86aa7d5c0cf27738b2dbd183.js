1e9521aa60923998ee99142b2dbe3da2347b0e53
j.bennett <61255945+jben817@users.noreply.github.com>
2020-08-13 17:08:14 -0400

    Updates

---
+++ precache-manifest.e018100c86aa7d5c0cf27738b2dbd183.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e4daaf679501a79f5b69bbe5e98c4e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e01b5f888717c0668d3e",
+    "url": "/react-portfolio/static/css/main.ce7b92c3.chunk.css"
+  },
+  {
+    "revision": "05d9fdf86a2a1d854d74",
+    "url": "/react-portfolio/static/js/2.2582b151.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-portfolio/static/js/2.2582b151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01b5f888717c0668d3e",
+    "url": "/react-portfolio/static/js/main.7c76d66a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "3b7fcc48452d24cb700a3d3df416dec5",
+    "url": "/react-portfolio/static/media/JB resume.3b7fcc48.pdf"
+  },
+  {
+    "revision": "6efcb5bc1d6e387dd81498fee41fe5e4",
+    "url": "/react-portfolio/static/media/NYME project photo.6efcb5bc.png"
+  },
+  {
+    "revision": "561623d92f0e11dd0da47b9c435c98c6",
+    "url": "/react-portfolio/static/media/day planner.561623d9.png"
+  },
+  {
+    "revision": "ee84f874959fd13602c5ff49013c604d",
+    "url": "/react-portfolio/static/media/generator website.ee84f874.png"
+  },
+  {
+    "revision": "4190a326e28f4143d626d7bb760c8a14",
+    "url": "/react-portfolio/static/media/oregon pic.4190a326.jpg"
+  },
+  {
+    "revision": "cd925278411e38acdab2038a9a167e59",
+    "url": "/react-portfolio/static/media/quiz screen cap.cd925278.png"
+  },
+  {
+    "revision": "120f85dc5b45adfe69b009e79cc828a7",
+    "url": "/react-portfolio/static/media/readme screen cap.120f85dc.png"
+  },
+  {
+    "revision": "81b29c745bf6685237f89878fa745a5c",
+    "url": "/react-portfolio/static/media/social streaming cap.81b29c74.png"
+  }
+]);
